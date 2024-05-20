3c76ee8d916581a714396f879d8b6f454550fef6
Udit Mehta <uditmehta100@gmail.com>
2024-05-20 14:06:37 +0530

    commit

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const PORT=8000;
 const bodyParser = require('body-parser');
 const connect=require('./connect');
 const urlRoutes=require('./routes/url');
-
+const path=require('path');
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
+const URL = require('./models/url');
 
 connect('mongodb://127.0.0.1:27017/short-url')
 .then(()=>console.log('Connected to MongoDB'))
 .catch(err=>console.error(err));
+
+app.set('view engine','ejs');
+app.set("views",path.resolve("./views"));
+
+app.get("/test",async (req,res)=>{
+    const urls = await URL.find({});
+    return res.render("home",{
+        urls:urls
+    });
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- routes/url.js
+++ routes/url.js
@@ -1,9 +1,47 @@
 const express=require('express');
 const router=express.Router();
 const shortid = require('shortid');
+const URL = require('../models/url');
 
-const {generateShortUrl} = require('../controllers/url');
+// const {generateShortUrl} = require('../controllers/url');
 
-router.post("/",generateShortUrl);
+router.post("/",async (req,res)=>{
+    const shortId=shortid(8);
+    console.log(req.body);
+    
+    if(!req.body.url){
+        return res.status(400).json({error:'url is required'});
+    }
+    console.log(req.body.url);
+    
+    await URL.create({
+        shortId:shortId,
+        rediectUrl:req.body.url,
+        visitHistory:[]
+    });
+
+    return res.json({id:shortId});
+});
+
+router.get("/:shortId", async (req,res)=>{
+
+    const url = await URL.findOne({shortId:req.params.shortId});
+    if(!url){
+        return res.status(404).json({error:'Url not found'});
+    }
+    url.visitHistory.push({timestamp:Date.now()});
+    await url.save();
+    return res.redirect(url.rediectUrl);
+});
+
+router.get("/analytics/:shortId", async (req,res)=>{
+
+    const shortId=req.params.shortId;
+    const url= await URL.findOne({shortId});
+    if(!url){
+        return res.status(404).json({error:'Url not found'});
+    }
+    return res.json({visitCount:url.visitHistory.length , analytics:url.visitHistory});
+})
 
 module.exports=router;
